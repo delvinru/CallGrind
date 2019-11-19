45f40af76f50c6821b90db85c835ed2af99598cd
Alexey <30408771+delvinru@users.noreply.github.com>
2019-11-19 12:51:48 +0300

    add input args for user programm

--- main.c
+++ main.c
@@ -9,9 +9,10 @@
     #define WIN 0
 #endif
 
-char* find_func_name(char* string, char* func_name);
-void build_file(FILE* file, FILE* source);
-char* create_name(char* name);
+char* find_func_name(char*, char*);
+void build_file(FILE*, FILE*);
+char* create_name(char*);
+char* create_command(char*, char*, char*);
 
 int main(int argc, char* argv[])
 {
@@ -24,48 +25,40 @@ int main(int argc, char* argv[])
     FILE* user_file;
     FILE* edited_file;
     char* file_name;
+    //Backup user_args
+    char user_args[128] = "";
+    if(argv[2] != NULL)
+        strcat(user_args, argv[2]);
+
+    //Base setting
     if(argv[1] != NULL)
     {
         user_file = fopen(argv[1],"r");
         file_name = create_name(argv[1]);
     } else{
-        printf("\e[1;31mUsage: callgrind path/to/file\e[0m\n");
+        printf("\e[1;31mUsage: callgrind sample/file args_for_you_program\e[0m\n");
         exit(1);
     }
+
+    //Create command
+    char buffer[128] = "";
+    if(strcmp(user_args, "") != 0)
+        create_command(buffer, file_name, user_args);
+    else
+        create_command(buffer, file_name, "");
+
     //Created backup file
     printf("\e[1;32m[+]\e[0mCreated file: %s\n", file_name);
     edited_file = fopen(file_name, "w");
+
     //Build edited file
     printf("\e[1;32m[+]\e[0mStart the analysis program\n");
     build_file(edited_file, user_file);
+
     //End of analysis
     printf("\e[1;32m[+]\e[0mAnalysis completed!\n");
     fclose(user_file);
     fclose(edited_file);
-
-    char buffer[128] = "";
-    if(strstr(file_name, ".cpp"))
-    {
-        strcat(buffer, "g++ ");
-        strcat(buffer, file_name);
-        strcat(buffer," headers/analyze.c -o output.out; ./output.out; rm output.out;");
-    }
-    else if(strstr(file_name, ".cs"))
-    {
-        printf("\e[1;31m[!]\e[0mSorry but now, I can only add_sensor to function, but not run this programm\n");
-        exit(0);
-    }
-    else if(strstr(file_name,".c"))
-    {
-        strcat(buffer, "gcc ");
-        strcat(buffer, file_name);
-        strcat(buffer," headers/analyze.c -o output.out; ./output.out; rm output.out;");
-    }
-    else{
-        printf("%s\n", file_name);
-        printf("\e[1;31m[!]\e[0mSorry work only with \e[1;37mC/C++/C#\e[0m files\n");
-        exit(1);
-    }
 
     printf("\e[1;32m[+]\e[0mExecutable command:%s\n", buffer);
     printf("\e[1;32m[+]\e[0mRun user program...\n");
@@ -76,6 +69,38 @@ int main(int argc, char* argv[])
     system("cat output.txt");
 
     return 0;
+}
+
+char* create_command(char* buffer, char* file_name, char* user_args)
+{
+    if(strstr(file_name, ".cpp"))
+    {
+        strcat(buffer, "g++ ");
+        strcat(buffer, file_name);
+        strcat(buffer, " ");
+        strcat(buffer,"headers/analyze.c -o output.out; ./output.out ");
+        strcat(buffer, user_args);
+        strcat(buffer, "; rm output.out");
+    }
+    else if(strstr(file_name, ".cs"))
+    {
+        printf("\e[1;31m[!]\e[0mSorry but now, I can only add_sensor to function, but not run this programm\n");
+        exit(0);
+    }
+    else if(strstr(file_name,".c"))
+    {
+        strcat(buffer, "gcc ");
+        strcat(buffer, file_name);
+        strcat(buffer, " ");
+        strcat(buffer,"headers/analyze.c -o output.out; ./output.out");
+        strcat(buffer,user_args);
+        strcat(buffer, "; rm output.out");
+    }
+    else{
+        printf("%s\n", file_name);
+        printf("\e[1;31m[!]\e[0mSorry work only with \e[1;37mC/C++/C#\e[0m files\n");
+        exit(1);
+    }
 }
 
 char* find_func_name(char* string, char* func_name)
@@ -178,7 +203,7 @@ void build_file(FILE* file, FILE* source)
             if(strstr(buffer, "{") || strstr(name_buf,"{"))
             {
                 find_func_name(buffer, function_name);
-                printf("\e[1;32m[+]\e[0mFunction \e[1;37m%s\e[0m find!\n", function_name);
+                printf("\e[1;32m[+]\e[0mFunction \e[1;33m%s\e[0m find!\n", function_name);
                 is_function = 1;
                 injected_sens = 0;
                 have_return = 0;
